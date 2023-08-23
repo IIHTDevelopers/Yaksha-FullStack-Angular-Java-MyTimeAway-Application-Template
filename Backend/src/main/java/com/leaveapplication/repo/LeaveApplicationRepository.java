8e467ee16197418855002abe58d80aa7a20f4029
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-08-23 16:02:54 +0530

    Delete Backend directory

--- Backend/src/main/java/com/leaveapplication/repo/LeaveApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leaveapplication.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.leaveapplication.entity.LeaveApplication;
-
-public interface LeaveApplicationRepository extends JpaRepository<LeaveApplication, Long> {
-
-}
